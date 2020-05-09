62ecf3e89363eccac2127f63968064af5ab2ab12
Finnesgard <50145916+efinite@users.noreply.github.com>
2020-05-08 23:37:08 -0500

    .count_items: argument fix

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,5 @@
 # Aggregate certain object types into a count
-.count_items <- function (x, na.rm) {
+.count_items <- function (x, na.rm = TRUE) {
   if (is.data.frame(x)) vctrs::vec_size(x) # data frame
   else if (!all(is.numeric(x))) {
     vctrs::vec_size(if (na.rm) stats::na.omit(x) else x) # Non-numeric
