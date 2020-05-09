91bca8fe8527826b6cee9e86501752ce2ed1994d
Finnesgard <50145916+efinite@users.noreply.github.com>
2020-05-08 20:33:57 -0500

    .count_items: vctrs

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,8 @@
 # Aggregate certain object types into a count
 .count_items <- function (x, na.rm) {
-  if (is.data.frame(x)) nrow(x)
-  else if (!all(is.numeric(x))) length(if (na.rm) stats::na.omit(x) else x)
-  else if (length(x) == 0) 0
-  else x
+  if (is.data.frame(x)) vctrs::vec_size(x) # data frame
+  else if (!all(is.numeric(x))) {
+    vctrs::vec_size(if (na.rm) stats::na.omit(x) else x) # Non-numeric
+  } else if (vctrs::vec_is_empty(x)) 0 # empty numeric
+  else x # Usable numeric
 }
